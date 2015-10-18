1e17c10d31f88db9721388c3c73e963d6341aa58
fzf <fanzhifangfzf@gmail.com>
2015-10-18 16:46:48 +0800

    correct the mistake

--- learn_git/main.cpp
+++ learn_git/main.cpp
@@ -10,6 +10,6 @@
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n"<<std::enl;
+    std::cout << "Hello, World!\n"<<std::endl;
     return 0;
 }
