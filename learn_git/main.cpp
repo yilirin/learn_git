38bf6937309aff9052cf01013e2c222835e82cda
fzf <fanzhifangfzf@gmail.com>
2015-10-18 16:44:22 +0800

    Initial Commit

---
+++ learn_git/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  learn_git
+//
+//  Created by 范志方 on 15/10/18.
+//  Copyright © 2015年 范志方. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
